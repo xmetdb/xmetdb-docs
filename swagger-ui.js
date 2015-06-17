79e8c2505794d7a6707961076709ad56b2740533
ngn <jeliazkova.nina@gmail.com>
2015-06-17 12:05:39 +0300

    Update swagger-ui.js

--- swagger-ui.js
+++ swagger-ui.js
@@ -1390,7 +1390,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
     
     HeaderView.prototype.showXmetdbAPI = function(e) {
       return this.trigger('update-swagger-ui', {
-        url: "http://ambit.uni-plovdiv.bg:8080/xmetdb/api-docs"
+        url: "http://xmetdb.org/xmetdb/api-docs"
       });
     };
     
